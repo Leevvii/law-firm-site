2e72374a6ed713ef39ee022b2aa71eb59a4c004b
Leevvii <floresjohnlevi@gmail.com>
2024-03-15 21:26:25 +0800

    Simplify the Navbar using map method

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,26 @@
 import { Link, NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const links = [
+    { label: "Home", to: "/" },
+    { label: "About", to: "/about" },
+    { label: "Services", to: "/services" },
+    { label: "Contact", to: "/contact" },
+  ];
+
   return (
     <>
       <header className="flex align-middle justify-between">
         <Link>Robredo Law Firm</Link>
         <nav>
           <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/services">Services</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
+            {links.map((link) => (
+              <li>
+                <NavLink key={link.to} to={link.to}>
+                  {link.label}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
         <button className="btn btn-primary">Get Started</button>
